d19d8953856cb69a1904df8d0d724e070b29ceb2
Ravindra Kumar <ravindrakumar15075@gmail.com>
2025-07-23 17:08:29 +0530

    Optimized Code

--- wavePrintMatrix.cpp
+++ wavePrintMatrix.cpp
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-#include <iostream>
+ #include <iostream>
 #include<vector>
 using namespace std;
  void wavePrintMatrix(vector<vector<int>>v){
@@ -29,36 +28,4 @@ vector<vector<int>>v {
  
  wavePrintMatrix(v);
     return 0;
-=======
-#include <iostream>
-#include<vector>
-using namespace std;
- void wavePrintMatrix(vector<vector<int>>v){
- int row = v.size();
- int col = v[0].size();
- for(int startCol = 0; startCol < col; startCol++){
-    //even number of col -> top to bottom 
-    if((startCol & 1) == 0){
-        for(int i = 0; i < row; i++){
-            cout << v[i][startCol] << " ";
-        }
-    }
-    else{
-        for(int i = row-1; i >= 0; i--){
-            cout << v[i][startCol]<<" ";
-        }
-    }
-    }
- }
-int main() {
-vector<vector<int>>v {
-    {1,2,3,4},
-    {5,6,7,8},
-    {9,10,11,12},
-   
-};
- 
- wavePrintMatrix(v);
-    return 0;
->>>>>>> aa9c5819495e8f0f75d760119478bc05a2866f02
 }
